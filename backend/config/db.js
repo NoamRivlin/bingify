00021b94ec5e2f7902c809d28464d9cf3e661c60
NoamRivlinLap <noam.rivlin15@gmail.com>
2023-04-12 14:49:36 +0300

    [deploy attempts]changes to backend package json for build

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
+  // console.log(process.env.MONGO_URI);
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(`DB connected ${connect.connection.host}`);
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config({ path: '../.env' });
 const PORT = process.env.PORT || 4141;
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
